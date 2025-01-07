733520f96a777a8561bff9c498f6ce1f68599603
deepakchoursiya <chourasiadeepak2002@gmail.com>
2025-01-07 12:40:08 +0530

    sub

--- Calculator.java
+++ Calculator.java
@@ -4,11 +4,21 @@ public static void main(Strig args[])
 {
      System.out.println("This is calculator");
      add(123, 45);
+     sub(100, 25);
+
+
 
 }
 public static add(int a , int s)
 {
   System.out.pritln(a+s);
 }
+
+public static sub(int a , int s)
+{
+  System.out.pritln(a-s);
+}
+
+
 
 }
