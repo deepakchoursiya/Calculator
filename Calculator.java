9e3de8c596e9c079b982cf79663b04544ebff0ac
deepakchoursiya <chourasiadeepak2002@gmail.com>
2025-01-07 13:24:17 +0530

    div

--- Calculator.java
+++ Calculator.java
@@ -5,20 +5,29 @@ public static void main(Strig args[])
      System.out.println("This is calculator");
      add(123, 45);
      sub(100, 25);
+     mul(23,66);
+	div(623,66);
 
 
 
 }
-public static add(int a , int s)
+public static void add(int a , int s)
 {
   System.out.pritln(a+s);
 }
 
-public static sub(int a , int s)
+public static void sub(int a , int s)
 {
   System.out.pritln(a-s);
 }
-
+public static void mul(int a , int s)
+{
+  System.out.pritln(a*s);
+}
+public static void div(int a , int s)
+{
+  System.out.pritln(a/s);
+}
 
 
 }
